b391c617e2153ea5f7391505d42149a38d69f1b5
Verga1 <fergal_mcgrath@amat.com>
2020-12-22 14:17:18 +0000

    added club views and templates

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Club
 
 # Create your views here.
+
+def all_clubs(request):
+    """ A view to show all classes, including sorting and search queries """
+
+    clubs = Club.objects.all()
+
+    context = {
+        'clubs': clubs,
+    }    
+
+    return render(request, 'clubs/clubs.html', context)
