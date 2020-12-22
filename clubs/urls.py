841d7a7ca5e11cc792d186e8dee59c1a0cf53191
Verga1 <fergal_mcgrath@amat.com>
2020-12-22 16:23:34 +0000

    added club details functionality

--- clubs/urls.py
+++ clubs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_clubs, name='clubs'),
+    path('<club_id>', views.club_detail, name='club_detail'),
 ]
--- clubs/views.py
+++ clubs/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Club
 
 # Create your views here.
 
 def all_clubs(request):
-    """ A view to show all classes, including sorting and search queries """
+    """ A view to show all clubs, including sorting and search queries """
 
     clubs = Club.objects.all()
 
@@ -13,3 +13,15 @@ def all_clubs(request):
     }    
 
     return render(request, 'clubs/clubs.html', context)
+
+
+def club_detail(request, club_id):
+    """ A view to show individual club details """
+
+    club = get_object_or_404(Club, pk=club_id)
+
+    context = {
+        'club': club,
+    }    
+
+    return render(request, 'clubs/club_detail.html', context)
