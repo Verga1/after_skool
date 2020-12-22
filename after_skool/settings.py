e891580de2c3879b25e78fc9d81da3ba26bcc1db
Verga1 <fergal_mcgrath@amat.com>
2020-12-22 20:24:04 +0000

    added shopping bag app urls and template

--- after_skool/settings.py
+++ after_skool/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'clubs',
+    'bag',
 ]
 
 MIDDLEWARE = [
--- after_skool/urls.py
+++ after_skool/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('clubs/', include('clubs.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
