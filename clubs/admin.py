dcf514383c1721e01e6427096316b42ce77827de
Verga1 <fergal_mcgrath@amat.com>
2020-12-22 13:51:13 +0000

    customized admin

--- clubs/admin.py
+++ clubs/admin.py
@@ -2,6 +2,34 @@ from django.contrib import admin
 from .models import Club, Category, Age
 
 # Register your models here.
-admin.site.register(Club)
-admin.site.register(Category)
-admin.site.register(Age)
+
+class ClubAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'age',
+        'teacher',
+        'price',
+        'term',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class AgeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Club, ClubAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Age, AgeAdmin)
