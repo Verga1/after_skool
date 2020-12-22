8c7c05840f371b4005e4ec2f82e32103ac54fa01
Verga1 <fergal_mcgrath@amat.com>
2020-12-22 16:44:09 +0000

    added search functionality

--- clubs/views.py
+++ clubs/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Club
 
 # Create your views here.
@@ -7,9 +9,22 @@ def all_clubs(request):
     """ A view to show all clubs, including sorting and search queries """
 
     clubs = Club.objects.all()
+    query = None
+
+    if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('clubs'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            clubs = clubs.filter(queries)
 
     context = {
         'clubs': clubs,
+        'search_term': query,
     }    
 
     return render(request, 'clubs/clubs.html', context)
