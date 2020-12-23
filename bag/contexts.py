929e96a2b20d8deb88ac80608ee0780524849466
Verga1 <fergal_mcgrath@amat.com>
2020-12-23 22:36:30 +0000

    updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from clubs.models import Club
 
 
 def bag_contents(request):
@@ -7,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     club_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        club = get_object_or_404(Club, pk=item_id)
+        total += quantity * club.price
+        club_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'club': club,
+        })
 
     if club_count >= settings.DISCOUNT_THRESHOLD:
         discount = total * Decimal(settings.STANDARD_DISCOUNT_PERCENTAGE / 100)
