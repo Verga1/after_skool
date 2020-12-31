0685932146ff7f8798fab6359839337deb81689b
Verga1 <fergal_mcgrath@amat.com>
2020-12-31 14:58:41 +0000

    added profile app

--- after_skool/settings.py
+++ after_skool/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'clubs',
     'bag',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
--- after_skool/urls.py
+++ after_skool/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('clubs/', include('clubs.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
