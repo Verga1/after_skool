86deaa1e3cbdb06c0a70cff1f659d2b2cf758f65
Verga1 <fergal_mcgrath@amat.com>
2020-12-31 14:26:45 +0000

    updated checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -50,7 +50,14 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            if request.POST.get('discount'):
+                print(f"DISCOUNT: {request.POST.get('discount')}")
+                order.discount = float(request.POST.get('discount'))
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     club = Club.objects.get(id=item_id)
