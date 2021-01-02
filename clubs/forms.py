6fb88be44265b57c8413089fccddd30eba88dfcc
Verga1 <fergal_mcgrath@amat.com>
2021-01-02 20:13:36 +0000

    Club admin - fixing the image field

--- clubs/forms.py
+++ clubs/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Club, Category
 
 
@@ -7,6 +8,8 @@ class ClubForm(forms.ModelForm):
     class Meta:
         model = Club
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
